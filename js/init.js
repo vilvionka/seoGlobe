2973cc1f5ca03522b06987c1199c07513e8cdfe8
vilivonka <gromootvod@lenta.ru>
2024-09-01 13:58:37 +0300

    final2

--- js/init.js
+++ js/init.js
@@ -43,3 +43,38 @@ $(function () {
   var accordion = new Accordion($('.question_accordion'), false);
 });
 
+
+
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var accordionHeader = this.el.find('.menu_box_elem');
+    accordionHeader.on('click', {
+      el: this.el,
+      multiple: this.multiple
+    },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      //this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.menu_box_wrap').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.menu_box'), false);
+});
+
+
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
   if (inp) {
 
-    for(let i = 0; i < inp.length; i++){
+    for (let i = 0; i < inp.length; i++) {
       inp[i].addEventListener('blur', function () {
         if (this.value == '') {
           this.closest('div').querySelector('span').classList.remove('remove');
@@ -14,7 +14,103 @@
         }
       })
     }
-    
+
   }
+
+})();
+
+
+// button 
+(() => {
+  let button = document.querySelectorAll('.js_button');
+
+  function buttonLeave() {
+    if (window.innerWidth < 371) {
+      for (let i = 0; i < button.length; i++) {
+        button[i].innerHTML = 'В ТОП!'
+      }
+    } else {
+      for (let i = 0; i < button.length; i++) {
+        button[i].innerHTML = 'Вывести в ТОП!'
+      }
+    }
+  }
+  buttonLeave();
+  window.addEventListener('resize', () => {
+    buttonLeave();
+  });
+
+})();
+
+// nav
+(() => {
+  let nav = document.querySelector('.header_flex_nav_wrap_elem span');
+  let navBox = document.querySelector('.header_flex_nav_wrap_box');
+
+  nav.addEventListener('click', function () {
+    if (navBox.classList.contains('active')) {
+      navBox.classList.remove('active');
+    } else {
+      navBox.classList.add('active');
+    }
+  })
+
+  document.addEventListener('click', (e) => {
+    const lkEl = e.composedPath().includes(nav);
+    const gambLk = e.composedPath().includes(navBox);
+    if (!lkEl && !gambLk) {
+      navBox.classList.remove('active');
+    }
+
+  })
+
+
+
+})();
+
+
+// menu mobile
+(() => {
+  let menu = document.querySelector('.js_menu');
+  let burger = document.querySelector('.js_burger');
+  let close = document.querySelector('.js_close')
+
+  burger.addEventListener('click', function () {
+    menu.classList.add('active');
+  })
+
+  close.addEventListener('click', function () {
+    menu.classList.remove('active');
+  })
+
+
+})();
+
+// copy button call
+(() => {
+  let copyCall = document.querySelector('#copy_call');
+
+  copyCall.addEventListener("click", function () {
+    navigator.clipboard.writeText(document.querySelector("#call_text").innerText).then(function () {
+      copyCall.innerText = 'Скопировано!';
+    }).catch(function (error) {
+      console.error('Error:', error);
+    });
+  });
+
+})();
+
+// copy button email
+
+(() => {
+  let copyEmail = document.querySelector('#copy_email');
+
+  copyEmail.addEventListener("click", function () {
+    navigator.clipboard.writeText(document.querySelector("#email_text").innerText).then(function () {
+      copyEmail.innerText = 'Скопировано!';
+    }).catch(function (error) {
+      console.error('Error:', error);
+    });
+  });
 
 })();
